410b728f752a66e1467b758af028c9c1b0a99ac7
nakamura6416 <102973038+nakamura6416@users.noreply.github.com>
2022-10-04 03:12:52 +0900

    Add portfolio file data upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+	$('.toggleBtn').on('click', function() {
+		if ($('#header').hasClass('open')) {
+			$('#header').removeClass('open');
+		} else {
+			$('#header').addClass('open');
+		}
+	});
+
+	$('#mask').on('click', function() {
+		$('#header').removeClass('open');
+	});
+});
